1baad3e88500eb761e6fb82e0ae5b52aa4d284b7
Your Name <your@email.com>
2025-09-02 16:03:51 +0530

    Fix image paths to work with Render deployment

--- src/Landing_Page/Home/Education.js
+++ src/Landing_Page/Home/Education.js
@@ -5,7 +5,7 @@ function Education() {
     <div className="container mt-5">
       <div className="row">
         <div className="col-6">
-          <img src="media/image/education.svg" alt="Education" style={{width:"90%"}}/>
+          <img src="/media/image/education.svg" alt="Education" style={{width:"90%"}}/>
         </div>
         <div className='col-6'>
           <h1 className='mb-3 fs-2'>Free and open market education</h1>
