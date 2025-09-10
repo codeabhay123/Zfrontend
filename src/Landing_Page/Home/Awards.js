d2615f90c629675bdb50e35be07972bc0af4f46d
Your Name <your@email.com>
2025-09-10 16:53:38 +0530

    v

--- src/Landing_Page/Home/Awards.js
+++ src/Landing_Page/Home/Awards.js
@@ -5,7 +5,7 @@ function Awards() {
         <div className='container mt-5'>
             <div className='row'>
                 <div className='col-6 p-5'>
-                    <img src='/media/Image/largestBroker.svg' alt='Largest'/>
+                    <img src='https://zerodha.com/static/images/ecosystem.png' alt='Largest'/>
                 </div>
                 
                 <div className='col-6 p-5'>
