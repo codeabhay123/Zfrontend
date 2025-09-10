2a535bc51bbd944a5858ea7cfcfe656ef4383168
Your Name <your@email.com>
2025-09-10 15:56:11 +0530

    first commit

--- src/Landing_Page/Pricing/Hero.js
+++ src/Landing_Page/Pricing/Hero.js
@@ -11,17 +11,17 @@ function Hero() {
       </div>
       <div className="row p-5 mt-5">
         <div className="col-4 p-5 text-center">
-          <img src="media/image/pricingEquity.svg" alt="zerors"/>
+          <img src="/media/image/pricingEquity.svg" alt="zerors"/>
           <h3>Free equity delivery</h3>
           <p className="text-muted">All equity delivery investments (NSE, BSE), are absolutely free — ₹ 0 brokerage.</p>
         </div>
         <div className="col-4 p-5">
-          <img src="media/image/intradayTrades.svg" alt="intra" />
+          <img src="/media/image/intradayTrades.svg" alt="intra" />
           <h3>Intraday and F&O trades</h3>
           <p className="text-muted">Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades.</p>
         </div>
         <div className="col-4 p-5">
-          <img src="media/image/pricingEquity.svg" alt="zerors"/>
+          <img src="/media/image/pricingEquity.svg" alt="zerors"/>
           <h3>Free direct MF</h3>
           <p className="text-muted">All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.</p>
         </div>
